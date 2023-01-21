b3a616a93cef861f56df4b9889a5f4b01ab4de35
sameh-dh <samehdh44@gmail.com>
2023-01-21 17:11:00 +0100

    Home starts here

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './css/App.css';
 import Header from './components/Header.jsx';
+import Home from './components/Home.jsx'
+
 function App() {
   return (
    <>
    <Header />
+   <main className='main'>
+    <Home />
+   </main>
    </>
   );
 }
