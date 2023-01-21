a3f25cde4ff2c1f8c41fe2a31676db70a4a7b55f
sameh-dh <samehdh44@gmail.com>
2023-01-21 17:35:59 +0100

    Data starts here

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import "../../css/Home.css"
+import Social from "./Social.jsx"
 
 const Home = () => {
   return (
 <section className="home section" id="home">
     <div className="home__container container grid">
    <div className="home__content grid">
+    <Social />
+     <div className="home_img"></div>
     
    </div>
     </div>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Social = () => {
   return (
-    <div>Social</div>
-  )
-}
+    <div className="home__socail">
+      <a href="https://www.instagram.com/butterfly_devo_o/" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+      </a>
 
-export default Social
+      <a href="https://www.linkedin.com/in/sameh-dhamer-5322b6195/" className="home__social-icon" target="_blank">
+        <i classNam="uil uil-linkedin-alt"></i> 
+      </a>
+
+      <a href="https://github.com/sameh-dh" className="home__social-icon" target="_blank">
+        <uil className="uil-github-alt"></uil>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
