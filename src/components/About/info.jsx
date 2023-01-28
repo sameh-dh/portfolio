dc3226ba4a918b76bcda6c40f515763117d9cfa4
sameh-dh <samehdh44@gmail.com>
2023-01-28 12:52:27 +0100

    organized design about component

--- src/components/About/info.jsx
+++ src/components/About/info.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="about__info grid">
+        <div className="about__box">
+        <i className='bx bx-award about__icon'></i>
+
+            <h3 className="about__title"> Experience </h3>
+                <span className="about__subtitle">
+                  6 Months Working
+                </span>
+          
+        </div>
+
+        <div className="about__box">
+        <i className='bx bx-briefcase-alt about__icon'></i>
+
+            <h3 className="about__title">Completed </h3>
+                <span className="about__subtitle ">
+                    8 + Projects
+                </span>
+         
+        </div>
+
+        <div className="about__box">
+        <i className='bx bx-support about__icon' ></i>
+
+            <h3 className="about__title"> Support </h3>
+                <span className="about__subtitle">
+                    Online 24/7
+                </span>
+      
+        </div>
+
+
+    </div>
   )
 }
 
