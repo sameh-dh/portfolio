9dde366bd7f1d1487b9409707ea035d22d2bcffd
sameh-dh <samehdh44@gmail.com>
2023-02-13 12:35:54 +0100

    qualification css to be done

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -2,9 +2,149 @@ import React from 'react'
 import "../../css/Qualification.css"
 
 const Qualification = () => {
-  return (
-    <div>Qualification</div>
-  )
+    return (
+        <section className="qualificaion section">
+            <h2 className="section__title">Qualification</h2>
+            <span className="section__subtitle">My personel journey</span>
+            <div className="qualification__container container">
+                <div className="qualification__tabs">
+                    <div className="qualification__button qualification__active button--flex">
+                        <i className="uil uil-graduation-cap qualification__icon"></i> Education
+                    </div>
+
+                    <div className="qualification__button button--flex">
+                        <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
+                    </div>
+                </div>
+
+                <div className="qualification__sections">
+                    <div className="qualification__content qualification__content-active">
+                        <div className="qualification__data">
+                            <div >
+                                <h3 className="qualification__title">Industrial Electrical</h3>
+                                <span className="qualification__subtitle">Iset - Bizerte</span>
+                                <div className="qualification__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - 2022
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div >
+                                <h3 className="qualification__title">Technical</h3>
+                                <span className="qualification__subtitle">Iset - Bizerte</span>
+                                <div className="qualification__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - 2022
+                                </div>
+                            </div>
+
+
+                        </div>
+
+
+                        <div className="qualification__data">
+                            <div >
+                                <h3 className="qualification__title">Industrial Electrical</h3>
+                                <span className="qualification__subtitle">Iset - Bizerte</span>
+                                <div className="qualification__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - 2022
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div >
+                                <h3 className="qualification__title">Web Developer</h3>
+                                <span className="qualification__subtitle">Rebootkamp - Tunis</span>
+                                <div className="qualification__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2022 - 2023
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                    <div className="qualification__content">
+                        <div className="qualification__data">
+                            <div >
+                                <h3 className="qualification__title">Hacker in Residence</h3>
+                                <span className="qualification__subtitle">Rbk - Tunis</span>
+                                <div className="qualification__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2022 - 2023
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div >
+                                <h3 className="qualification__title">Technical</h3>
+                                <span className="qualification__subtitle">Iset - Bizerte</span>
+                                <div className="qualification__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - 2022
+                                </div>
+                            </div>
+
+
+                        </div>
+
+
+                        <div className="qualification__data">
+                            <div >
+                                <h3 className="qualification__title">Industrial Electrical</h3>
+                                <span className="qualification__subtitle">Iset - Bizerte</span>
+                                <div className="qualification__calender">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - 2022
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                      
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Qualification
