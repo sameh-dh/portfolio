b147dbb247871360f8353b5e7e9d386fed5651ae
sameh-dh <samehdh44@gmail.com>
2023-02-21 12:09:44 +0100

    projects done for large medium and small devices

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,11 +1,38 @@
 import Work1 from "../../images/work1.jpg"
-
+import Work2 from "../../images/work2.jpg"
+import Work3 from "../../images/work3.jpg"
+import Work4 from "../../images/work4.jpg"
+import Work5 from "../../images/work5.jpg"
 export const projectData = [
     {
         id: 1,
         image: Work1,
         title: "Web Design",
         category: "web"
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Web Design",
+        category: "app"
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Web Design",
+        category: "web"
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Web Design",
+        category: "web"
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "Web Design",
+        category: "app"
     }
 ]
 
--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -1,24 +1,48 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import { projectData, ProjectNav } from './Data'
 import WorksItem from './WorksItem'
 const Works = () => {
+    const [item, setItem] = useState({ name: "all" })
+    const [projects, setProjects] = useState([])
+    const [active, setActive] = useState(0);
+
+    useEffect(() => {
+        if (item.name === "all") {
+            setProjects(projectData)
+        }
+        else {
+            const newProjects = projectData.filter(project => {
+                return project.category.toLowerCase() === item.name;
+            });
+            setProjects(newProjects)
+        }
+    }, [item]);
+
+
     return (
-       <div>
-         <div className="work__filters">
-            {ProjectNav.map((item, index) => {
-                return (
-                    <span className="work__item" key={index}>{item.name} </span>
-                )
-            })}
+        <div>
+            <div className="work__filters">
+                {ProjectNav.map((item, index) => {
+                    return (
+                        <span onClick={() => {
+                            setItem({ name: item.name.toLocaleLowerCase() });
+                            setActive(index);
+                        }}
+                            className={`${active === index ? 'active-work' : ""} work__item`} key={index}
+                            > {item.name} </span>
+                    )
+                })}
+            </div>
+            <div className="work__container container grid">
+                {
+                    projects.map((item, index) => {
+                        return <WorksItem item={item} key={item.id} />
+                    })
+                }
+            </div>
         </div>
-        <div className="work__container container grid">
-            {
-            projectData.map((item, index) => {
-            return <WorksItem />
-            })
-            }
-        </div>
-       </div>
     )
 }
 
